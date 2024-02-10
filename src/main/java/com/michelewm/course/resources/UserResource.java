caf25e4050021158376d9220ec1cf5154dc52ee4
michelebswm <michele.fenix.17@gmail.com>
2024-02-10 20:40:37 -0300

    feat: User entity and resource

---
+++ src/main/java/com/michelewm/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.michelewm.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.michelewm.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Michele", "michele@gmail.com", "33412281", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
