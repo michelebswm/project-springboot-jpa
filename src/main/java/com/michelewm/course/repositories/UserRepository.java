59c7fb77ff3703a056f1213880c6c8ac37ea64ce
michelebswm <michele.fenix.17@gmail.com>
2024-02-11 01:23:50 -0300

    feat: JPA repository, dependecy injection, database seeding

---
+++ src/main/java/com/michelewm/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.michelewm.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.michelewm.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
