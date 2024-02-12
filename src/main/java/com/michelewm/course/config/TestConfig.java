011e5752f855cd0e1cabaa6ad43d77e04f46bf60
michelebswm <michele.fenix.17@gmail.com>
2024-02-12 20:12:37 -0300

    feat: created class Payment, one-to-one association

--- src/main/java/com/michelewm/course/config/TestConfig.java
+++ src/main/java/com/michelewm/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.michelewm.course.entities.Category;
 import com.michelewm.course.entities.Order;
 import com.michelewm.course.entities.OrderItem;
+import com.michelewm.course.entities.Payment;
 import com.michelewm.course.entities.Product;
 import com.michelewm.course.entities.User;
 import com.michelewm.course.entities.enums.OrderStatus;
@@ -86,6 +87,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 }
