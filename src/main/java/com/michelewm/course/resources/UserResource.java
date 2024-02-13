94238a18948b3b81379f5750772a2c1e55de8d38
michelebswm <michele.fenix.17@gmail.com>
2024-02-13 06:59:38 -0300

    feat: Exception handling - findById

--- src/main/java/com/michelewm/course/resources/UserResource.java
+++ src/main/java/com/michelewm/course/resources/UserResource.java
@@ -55,6 +55,7 @@ public class UserResource {
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<User> update(@PathVariable Long id,@RequestBody User obj){
 		obj = service.update(id, obj);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(obj); //Return API 200OK
+
 	}
 }
